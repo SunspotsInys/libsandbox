593ca33543d8854942a095fb8fd610967cf36bdf
ggaaooppeenngg <471160171@qq.com>
2014-07-20 19:32:06 +0800

    test

--- sandbox/judge_test.go
+++ sandbox/judge_test.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os/exec"
 	"testing"
 )
 
 func TestJudge(t *testing.T) {
-	cmd := exec.Command("sandbox", "--lang=c", "../test/judge/a+b.c", "../test/judge/a+b", "..test/judge/input", "../test/judge/output")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		println(err)
-	}
-	t.Log(out.Bytes())
-	if fmt.Sprintf("%s", out.Bytes()) != "AC" {
-		t.Log(err)
+	cmd := exec.Command("sandbox", "--lang=c", "judge/src/A1/a+b.c", "judge/binary/A1/a+b", "judge/src/A1/input", "judge/src/A1/output")
+	out, _ := cmd.CombinedOutput()
+	t.Logf("%s", out)
+	if fmt.Sprintf("%s", out) != "AC" {
 		t.Fatal("wrong answer")
 	}
 }
