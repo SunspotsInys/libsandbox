bb0354df79dce771cd421982f1349fd81bede90f
ggaaooppeenngg <471160171@qq.com>
2014-09-07 21:58:36 +0800

    modiry

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -11,27 +11,63 @@ import (
 	"github.com/ggaaooppeenngg/sandbox"
 )
 
-func checkStatus(obj *sandbox.RunningObject) {
+const (
+	BINARY  = "binary"
+	COMPILE = "compile"
+	SOURCE  = "source"
+	TIME    = "time"
+	MEMORY  = "memory"
+	INPUT   = "input"
+	OUTPUT  = "output"
+	LANG    = "lang"
+
+	C   = "c"
+	CPP = "cpp"
+	GO  = "go"
+
+	DELIM = "!-_-\n"
+)
+
+func panicErr(e error) {
+	if e != nil {
+		fmt.Println(e)
+	}
+}
+
+//read byte from the file
+func readFile(f *os.File) (testOut []byte) {
+	tmp := make([]byte, 256)
+	for n, err := f.Read(tmp); err != io.EOF; n, err = f.Read(tmp) {
+		testOut = append(testOut, tmp[:n]...)
+	}
+	return testOut
+}
+
+//obj records process information and n is the nth test,if n is 0 ,all test are passed
+func checkStatus(obj *sandbox.RunningObject, n int) {
 	switch obj.Status {
 	case sandbox.AC:
-		fmt.Printf("AC:%d:%d", obj.Memory, obj.Time)
+		fmt.Printf("AC:%d:%d:%d", obj.Memory, obj.Time, n)
 	case sandbox.MLE:
-		fmt.Printf("MLE:%d:%d", obj.Memory, obj.Time)
+		fmt.Printf("MLE:%d:%d:%d", obj.Memory, obj.Time, n)
 	case sandbox.TLE:
-		fmt.Printf("TLE:%d:%d", obj.Memory, obj.Time)
+		fmt.Printf("TLE:%d:%d:%d", obj.Memory, obj.Time, n)
 	case sandbox.WA:
-		fmt.Printf("WA:%d:%d", obj.Memory, obj.Time)
+		fmt.Printf("WA:%d:%d:%d", obj.Memory, obj.Time, n)
 	default:
-		fmt.Printf("FE:%d:%d", obj.Memory, obj.Time)
+		fmt.Printf("FE:%d:%d:%d", obj.Memory, obj.Time, n)
 	}
 }
 
 func main() {
+
 	app := cli.NewApp()
 	app.Name = "sandbox"
 	app.Usage = `test untrusted source code'
 example:
-	sandbox --lang=c src/main.c bin/main judge/input judge/output
+	sandbox --lang=c -c -s src/main.c -b bin/main --memory=10000 --time=1000 --input=judge/input --output==judge/output
+	sandbox --lang=c bin/main --input=judge/input --output=judge/output
+	echo "input" | sandbox --lang=c  bin/main 
 result:
 	status:time:memory`
 	app.Author = "ggaaooppeenngg"
@@ -41,101 +77,104 @@ result:
 		cli.IntFlag{Name: "time,t", Value: 1000, Usage: "time limit in MS"},
 		cli.IntFlag{Name: "memory,m", Value: 10000, Usage: "memory limit in KB"},
 		cli.BoolFlag{Name: "compile,c", Usage: "wether complie before running", EnvVar: ""},
+		cli.StringFlag{Name: "input,i", Value: "", Usage: "input file path"},
+		cli.StringFlag{Name: "output,o", Value: "", Usage: "output file path"},
+		cli.StringFlag{Name: "source,s", Value: "", Usage: "source file path"},
+		cli.StringFlag{Name: "binary,b", Value: "", Usage: "binary file path"},
 	}
 	app.Action = func(c *cli.Context) {
-		var bin string
-		var inPath string
-		var outPath string
+		var in *os.File //input file instance
+		var src string  // source file path
+		var bin string  //binary file path
 		var err error
-		var in *os.File
 		pwd, err := os.Getwd()
 		if err != nil {
 			panic(err)
 		}
-		if c.Bool("compile") {
-			if len(c.Args()) < 1 {
-				println("not match the number of arguments,please use -h to help.")
-				return
+		//target binary file path is neccessary
+		if c.String(BINARY) != "" {
+			p := c.String(BINARY)
+			if path.IsAbs(p) {
+				bin = p
 			} else {
-				bin = path.Join(pwd, c.Args()[1])
-				if len(c.Args()) >= 4 {
-					outPath = path.Join(pwd, c.Args()[3])
-				}
-				if len(c.Args()) >= 3 {
-					inPath = path.Join(pwd, c.Args()[2])
-					in, err = os.Open(inPath)
-				} else {
-					in, err = os.Open(os.DevNull)
-				}
+				bin = path.Join(pwd, p)
 			}
 		} else {
-			if len(c.Args()) < 2 {
-				println("not match the number of arguments,please use -h to help.")
-				return
+			println("needs target binary file path as argument")
+			return
+		}
+
+		//if input is not set , use /dev/null as input
+		if c.String(INPUT) == "" {
+			in, err = os.Open(os.DevNull)
+		} else {
+			p := c.String(INPUT)
+			if path.IsAbs(p) {
+				in, err = os.Open(p)
 			} else {
-				bin = path.Join(pwd, c.Args()[0])
-				if len(c.Args()) >= 3 {
-					outPath = path.Join(pwd, c.Args()[2])
-				}
-				if len(c.Args()) >= 2 {
-					inPath = path.Join(pwd, c.Args()[1])
-					in, err = os.Open(inPath)
-				} else {
-					in, err = os.Open(os.DevNull)
-				}
+				in, err = os.Open(path.Join(pwd, p))
 			}
 		}
-		time := int64(c.Int("time"))
-		memory := int64(c.Int("memory"))
-
 		if err != nil {
 			panic(err)
 		}
 		defer in.Close()
-		var out bytes.Buffer
-		var obj = &sandbox.RunningObject{}
 
-		//compile code ,if compile set , not compile
-		if c.Bool("compile") {
-			if c.String("lang") == "c" {
-				if err = sandbox.Complie(c.Args()[0], c.Args()[1], sandbox.C); err != nil {
-					fmt.Printf("CE:0:0")
-					return
-				}
-			}
-			if c.String("lang") == "cpp" {
-				if err = sandbox.Complie(c.Args()[0], c.Args()[1], sandbox.CPP); err != nil {
-					fmt.Printf("CE:0:0")
-					return
-				}
-			}
-			if c.String("lang") == "go" {
-				if err = sandbox.Complie(c.Args()[0], c.Args()[1], sandbox.GO); err != nil {
-					fmt.Printf("CE:0:0")
-					return
+		if c.Bool(COMPILE) {
+			if c.String(SOURCE) == "" {
+				println("compiler needs source file!")
+				return
+			} else {
+				//compile code ,if compile set , not compile
+				if c.Bool(COMPILE) {
+					var language uint64
+					switch c.String(LANG) {
+					case C:
+						language = sandbox.C
+					case CPP:
+						language = sandbox.CPP
+					case GO:
+						language = sandbox.GO
+					}
+					if err = sandbox.Complie(src, bin, language); err != nil {
+						fmt.Printf("CE:0:0")
+						return
+					}
 				}
 			}
 		}
-		//fmt.Println(bin, inPath, outPath)
-		obj = sandbox.Run(bin, in, &out, []string{"tmp"}, time, memory)
-		if outPath != "" {
+
+		var obj = &sandbox.RunningObject{}
+		time := int64(c.Int(TIME))
+		memory := int64(c.Int(MEMORY))
+		if c.String(OUTPUT) != "" {
+			//get input tests and run every test one by one
+			i := readFile(in)
+			inputs := bytes.Split(i, []byte(DELIM))
+			//get out test and check if every output matches the single input
+			outPath := c.String(OUTPUT)
 			//get output
-			outFile, err := os.Open(outPath)
-			defer outFile.Close()
+			out, err := os.Open(outPath)
 			if err != nil {
 				panic(err)
 			}
-			var testOut []byte
-			tmp := make([]byte, 256)
-			for n, err := outFile.Read(tmp); err != io.EOF; n, err = outFile.Read(tmp) {
-				testOut = append(testOut, tmp[:n]...)
-			}
-			if bytes.Equal(out.Bytes(), testOut) {
-				fmt.Printf("AC:%d:%d", obj.Memory, obj.Time)
-				return
+			defer out.Close()
+			o := readFile(out)
+			outputs := bytes.Split(o, []byte(DELIM))
+			for i, v := range inputs {
+				inBytes := bytes.NewBuffer(v)
+				out := bytes.Buffer{}
+				o := sandbox.Run(bin, inBytes, &out, []string{""}, time, memory)
+				if bytes.Equal(out.Bytes(), outputs[i]) {
+					obj.Time += o.Time
+					obj.Memory += o.Memory
+				} else {
+					checkStatus(o, i)
+				}
 			}
 		}
-		checkStatus(obj)
+
+		checkStatus(obj, 0)
 	}
 	app.Run(os.Args)
 }
