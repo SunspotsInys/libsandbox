165d5bc3cd055c99497f50e055638fd2955fb3a6
ggaaooppeenngg <471160171@qq.com>
2015-02-17 20:50:37 +0800

    add ptrace and cont syscall having bug

--- limit_test.go
+++ limit_test.go
@@ -5,13 +5,16 @@ import (
 	"os/exec"
 	"syscall"
 	"testing"
-
-	"golang.org/x/sys/unix"
 )
 
 func TestCPULimit(t *testing.T) {
 	c := exec.Command("test/main")
 
+	c.Stdout = os.Stdout
+	c.SysProcAttr = &syscall.SysProcAttr{
+		Ptrace:  true,
+		Setpgid: true, // progress group ,killed when the leader process killed.
+	}
 	c.Start()
 
 	err := setTimelimit(c.Process.Pid, 3)
@@ -28,23 +31,31 @@ func TestMemoryLimit(t *testing.T) {
 	c := exec.Command("test/test")
 	c.Stdout = os.Stdout
 	c.SysProcAttr = &syscall.SysProcAttr{
+		Ptrace:  true,
 		Setpgid: true,
 	}
 	err := c.Start()
 	if err != nil {
 		t.Fatal(err)
 	}
-	// sometime
+
 	err = setMemLimit(c.Process.Pid, 1024*512)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// continue the execution.
+	err = syscall.PtraceCont(c.Process.Pid, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	err = c.Wait()
 
 	// soemtimes got exit status 127,sometimes got segment fault,not know why.
-
 	if err == nil {
 		t.Fatal("memory limit test failed")
 	}
+	t.Log(err)
 
 }
